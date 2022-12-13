a883ef8608e5f302d2809a1bde0dbb720973733c
faze20 <42577269+faze20@users.noreply.github.com>
2022-12-13 10:35:30 -0800

    graph2

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -3,9 +3,18 @@ import Coin from '../../../assets/data/crypto.json'
 import CoinDetailedHeader from './components/CoinDetailedHeader'
 import styles from './styles'
 import { AntDesign } from '@expo/vector-icons';
-import {ChartDot, ChartPath, ChartPathProvider} from '@rainbow-me/animated-charts';
+import {ChartDot, ChartPath, ChartPathProvider, monotoneCubicInterpolation} from '@rainbow-me/animated-charts';
 
 
+export const data = [
+    {x: 1453075200, y: 1.47}, {x: 1453161600, y: 1.37},
+    {x: 1453248000, y: 1.53}, {x: 1453334400, y: 1.54},
+    {x: 1453420800, y: 1.52}, {x: 1453507200, y: 2.03},
+    {x: 1453593600, y: 2.10}, {x: 1453680000, y: 2.50},
+    {x: 1453766400, y: 2.30}, {x: 1453852800, y: 2.42},
+    {x: 1453939200, y: 2.55}, {x: 1454025600, y: 2.41},
+    {x: 1454112000, y: 2.43}, {x: 1454198400, y: 2.20},
+  ];
 
 const CoinDetailedScreen = () => {
     const {
@@ -22,13 +31,18 @@ const CoinDetailedScreen = () => {
 
     const percentageColor = price_change_percentage_24h < 0 ? '#ea3943': '#16c784';
     const screenWidth = Dimensions.get('window').width;
+    const points = monotoneCubicInterpolation({data, range: 40});
+
 
   return (
     <View style={{ paddingHorizontal: 10}}>
-        <ChartPathProvider data={{
-             points:prices.map(([x,y])=>({x , y})),
-              smoothingStrategy: 'bezier' ,
-              }}>
+        <ChartPathProvider 
+            data={{ points, smoothingStrategy: 'bezier' }}
+            // data={{
+            //     points:prices.map(([x,y])=>({x , y})),
+            //     smoothingStrategy: 'bezier' ,
+            //     }}
+                >
             <CoinDetailedHeader
             image={small}
             symbol={symbol}
